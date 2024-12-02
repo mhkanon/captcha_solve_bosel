5cbeb7774e44b1cf7d0390c4a6087bddd4e8ef4e
Mehedi Hasan <mhkanon@gmail.com>
2024-12-02 11:41:34 +0600

    Update cap_python_code.py

--- cap_python_code.py
+++ cap_python_code.py
@@ -13,6 +13,7 @@ CORS(app)
 
 # Configure Chrome options (no headless mode for debugging)
 chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
